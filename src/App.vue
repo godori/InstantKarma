d500a835834f5158ffac895a829acda805a2870b
godori <godori@godoriui-MacBook-Pro.local>
2017-11-06 23:45:48 +0900

    add vue cli

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>INSTANT KARMA</h1>
+    <input type="checkbox" name="" value="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
